97d847b98c84ac9e7f53fb7ba9595b33facc7d79
Franklin <franklin.zhang@qq.com>
2023-03-29 00:19:53 +0800

    pyautogui screenshots without save screenshots

--- screenshots.py
+++ screenshots.py
@@ -1,11 +1,12 @@
 import pyautogui
-import argparse
 import cv2
-
+import numpy as np
 
 def take_shots(args):
     path = args.dir + "/screenshots/screenshot.png"
-    pyautogui.screenshot(path)
-    img = cv2.imread(path)
+
+    img=pyautogui.screenshot()
+    img=np.array(img)
+    
     img = cv2.resize(img, (0, 0), fx=args.resize, fy=args.resize)
-    cv2.imwrite(path, img)
+    return img
