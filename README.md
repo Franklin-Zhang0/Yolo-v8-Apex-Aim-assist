2ce910eaccc135eb71017dda16c13d41ebd70567
Franklin <franklin.zhang@qq.com>
2023-04-12 23:10:06 +0800

    Models updated

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ if you have a cuda capable gpu, you can running the following extra command
 
 `pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116`
 
+### Using TensorRT to accelerate
+Add the following command
+```
+pip install --upgrade setuptools pip --user
+pip install nvidia-pyindex
+pip install --upgrade nvidia-tensorrt
+pip install pycuda
+```
+
 ## How to run the program
 just run the main.py file with the following command
 
@@ -25,6 +34,10 @@ You can change the settings in the `args.py` file.
 would jump from side to side.
 - model
     - The default model is for Apex. However, you can train your own model using train.py, and switch the model using this setting.
+    - There're several model in the "model" dir, you can choose one of them.
+        - The `.trt` models are for tensorRT, which is about 4 times faster than the `.pt` models, but with the same accuracy. 
+        - Model speed: `8n>8s>8m`
+        - Model accuracy: `8n<8s<8m`
 - crop_size
     - This setting determines the portion of the screen to be detected. Too high may cause difficulty in detecting little objects.
 ## Note
--- args_.py
+++ args_.py
@@ -11,13 +11,13 @@ def arg_init(args):
         "--model_dir", type=str, default=dirpath + "/model", help="model dir"
     )
     args.add_argument("--model", type=str,
-                      default="/apex_yolov6-8n-376.pt", help="model path")
+                      default="/apex_8n.trt", help="model path")
     args.add_argument("--iou",type=float,
                       default=0.8,help="predict iou")
     args.add_argument("--conf",type=float,
                         default=0.6,help="predict conf")
     args.add_argument("--crop_size", type=float,
-                      default=1/3, help="the portion to detect from the screen(=crop_window_height/screen_height)(It's always a rectangle)(from 0 to 1)")
+                      default=1/2, help="the portion to detect from the screen(=crop_window_height/screen_height)(It's always a rectangle)(from 0 to 1)")
     args.add_argument("--wait", type=float, default=0, help="wait time")
     args.add_argument("--verbos", type=bool, default=False, help="predict verbos")
     args.add_argument("--target_index", type=int,
@@ -30,11 +30,11 @@ def arg_init(args):
                       default=5,help="how smooth the mouse move(from 1. to 5.)")
     args.add_argument("--game_fps", type=int, default=60, help="the fps in the game")
 
-    #PID args
+    # PID args
     args.add_argument("--pid", type=bool, default=True, help="use pid")
-    args.add_argument("--Kp", type=float, default=0.4, help="Kp")
+    args.add_argument("--Kp", type=float, default=0.25, help="Kp")
     args.add_argument("--Ki", type=float, default=0.05, help="Ki")
-    args.add_argument("--Kd", type=float, default=0.25, help="Kd")
+    args.add_argument("--Kd", type=float, default=0.2, help="Kd")
 
     args = args.parse_args(args=[])
     return args
